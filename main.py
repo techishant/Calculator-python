3143177a241687d52b861e67d0c9780ece650297
TECHISHANT <74040166+techishant@users.noreply.github.com>
2021-07-23 21:22:37 +0530

    added float instead int and  function instead elif
    Just changed the int to float in inputs so user can calculate decimal values also.
    Also changed operations from elif to functions.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+# some basic needs
 ansTag = 'ans>> '
-
-def main():
-    while(True):
-        inputDATA = str(input(
-'''
+infoText = '''
 1). Add
 2). Subtract
 3). Multiply
@@ -12,7 +9,41 @@ def main():
 0). exit
 ?). ---help for help
 >>  '''
-        )).upper().replace(' ', '')
+
+# functions for the operations
+def sum():
+    num1 = float(input('First Num >>  '))
+    num2 = float(input('Second Num >>  '))
+    ans = input(ansTag + str(num1+num2))
+
+def sub():
+    num1 = float(input('First Num >>  '))
+    num2 = float(input('Second Num >>  '))
+    ans = input(ansTag + str(num1-num2))
+
+def mul():
+    num1 = float(input('First Num >>  '))
+    num2 = float(input('Second Num >>  '))
+    ans = input(ansTag + str(num1*num2))
+
+def div1():
+    num1 = float(input('First Num >>  '))
+    num2 = float(input('Second Num >>  '))
+    if num2 == 0:
+        input("Can't divide by 0")
+    else:
+        ans = input(ansTag + str(num1/num2))
+
+def div2():
+    num1 = float(input('First Num >>  '))
+    num2 = float(input('Second Num >>  '))
+    ans = input(ansTag + 'Quotient: ' + str(int(num1/num2)) + '\n' + ansTag + 'Remainder: ' + str(num1%num2))
+
+
+def main():
+    print('\n ******* Paculator *******')
+    while(True):
+        inputDATA = str(input(infoText)).upper().replace(' ', '')
 
 # <----------------------------- DANGER  ---------------------------------->
         if (inputDATA == 'EXIT' or inputDATA == '0'):
@@ -30,35 +61,22 @@ Nothing's Available Here for Now....
 '''
             )
 # <------------------------------------------------------------------------>
-
+# conditons for the operations to be called
 
         elif(inputDATA == 'ADD' or inputDATA == '1' or inputDATA == '+'):
-            num1 = int(input('First Num >>  '))
-            num2 = int(input('Second Num >>  '))
-            ans = input(ansTag + str(num1+num2))
+            sum()
 
         elif(inputDATA == 'SUBTRACT' or inputDATA == 'SUB' or inputDATA == '2' or inputDATA == '-' or inputDATA == '_'):
-            num1 = int(input('First Num >>  '))
-            num2 = int(input('Second Num >>  '))
-            ans = input(ansTag + str(num1-num2))
+            sub()
             
         elif(inputDATA == 'MULTIPLY' or inputDATA == 'MUL' or inputDATA == '3' or inputDATA == '*'):
-            num1 = int(input('First Num >>  '))
-            num2 = int(input('Second Num >>  '))
-            ans = input(ansTag + str(num1*num2))
+            mul()
 
         elif(inputDATA == 'DIVIDE' or inputDATA == 'DIVIDE1' or inputDATA == 'DIV' or inputDATA == 'DIV1' or inputDATA == '4' or inputDATA == '/'):
-            num1 = int(input('First Num >>  '))
-            num2 = int(input('Second Num >>  '))
-            if num2 == 0:
-                input("Can't divide by 0")
-            else:
-                ans = input(ansTag + str(num1/num2))
+            div1()
 
         elif(inputDATA == 'DIVIDE1' or inputDATA == 'DIV1' or inputDATA == '5' or inputDATA == '!/' or inputDATA == '/1'):
-            num1 = int(input('First Num >>  '))
-            num2 = int(input('Second Num >>  '))
-            ans = input(ansTag + 'Quotient: ' + str(int(num1/num2)) + '\n' + ansTag + 'Remainder: ' + str(num1%num2))
+            div2()
             
         
 main()
