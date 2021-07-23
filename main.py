c1b3f3dc292b37e9b1c0c9e3e3e6b812e117bb2b
TECHISHANT <74040166+techishant@users.noreply.github.com>
2021-07-23 22:11:14 +0530

    Added Multiplication Table feature + Bug Fixes
    Added a multiplication table where if you type something among below You trigger the multiplication table-->
    >>  6,multab,multiplicationtable,tab.table
    
    $ Bug fixes
    fixed a bug where on dividing two numbers. for eg -> 8/3
    gives output 
    >> Quotient - 2
    >> Remainder - 2.0
    
    is now fixed and gives output -->
    >> Quotient - 2
    >> Remainder - 2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ infoText = '''
 3). Multiply
 4). Divide1  (decimal)
 5). Divide2  (quotient and remainder)
+6). Multiplication Table  (multab)
 0). exit
 ?). ---help for help
 >>  '''
@@ -37,8 +38,19 @@ def div1():
 def div2():
     num1 = float(input('First Num >>  '))
     num2 = float(input('Second Num >>  '))
-    ans = input(ansTag + 'Quotient: ' + str(int(num1/num2)) + '\n' + ansTag + 'Remainder: ' + str(num1%num2))
+    ans = input(ansTag + 'Quotient: ' + str(int(num1/num2)) + '\n' + ansTag + 'Remainder: ' + str(int(num1%num2)))
 
+def mulTab():
+    i = 1
+    num = float(input('Number >>  '))
+    print("Multiplication table for ==> ",int(num) )
+    while(not i > 10):
+        print('>>  ', int(num), ' * ' , i , ' = ', int(num)*i)
+        i = i+1
+    buffer = input()
+    
+
+# ----------------------------------------------------------------------------
 
 def main():
     print('\n ******* Paculator *******')
@@ -61,22 +73,22 @@ Nothing's Available Here for Now....
 '''
             )
 # <------------------------------------------------------------------------>
+
+
 # conditons for the operations to be called
 
         elif(inputDATA == 'ADD' or inputDATA == '1' or inputDATA == '+'):
             sum()
-
         elif(inputDATA == 'SUBTRACT' or inputDATA == 'SUB' or inputDATA == '2' or inputDATA == '-' or inputDATA == '_'):
             sub()
-            
         elif(inputDATA == 'MULTIPLY' or inputDATA == 'MUL' or inputDATA == '3' or inputDATA == '*'):
             mul()
-
         elif(inputDATA == 'DIVIDE' or inputDATA == 'DIVIDE1' or inputDATA == 'DIV' or inputDATA == 'DIV1' or inputDATA == '4' or inputDATA == '/'):
             div1()
-
         elif(inputDATA == 'DIVIDE1' or inputDATA == 'DIV1' or inputDATA == '5' or inputDATA == '!/' or inputDATA == '/1'):
             div2()
+        elif(inputDATA == '6'or inputDATA == 'MULTAB' or inputDATA == 'MULTIPLICATIONTABLE' or inputDATA == 'TAB' or inputDATA == 'TABLE'):
+            mulTab()
             
         
 main()
